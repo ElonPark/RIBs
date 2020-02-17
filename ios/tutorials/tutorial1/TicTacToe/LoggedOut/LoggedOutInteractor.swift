0c9d896a35ecf15867395def22fe8ee50be344b7
Elon <dev.sungwoon@gmail.com>
2020-02-18 03:20:47 +0900

    :sparkles: 튜토리얼1 완료

---
+++ ios/tutorials/tutorial1/TicTacToe/LoggedOut/LoggedOutInteractor.swift
@@ -0,0 +1,64 @@
+//
+//  LoggedOutInteractor.swift
+//  TicTacToe
+//
+//  Created by Elon on 2020/02/18.
+//  Copyright © 2020 Uber. All rights reserved.
+//
+
+import RIBs
+import RxSwift
+
+protocol LoggedOutRouting: ViewableRouting {
+    // TODO: Declare methods the interactor can invoke to manage sub-tree via the router.
+}
+
+protocol LoggedOutPresentable: Presentable {
+    var listener: LoggedOutPresentableListener? { get set }
+    // TODO: Declare methods the interactor can invoke the presenter to present data.
+}
+
+protocol LoggedOutListener: class {
+    // TODO: Declare methods the interactor can invoke to communicate with other RIBs.
+}
+
+final class LoggedOutInteractor: PresentableInteractor<LoggedOutPresentable>, LoggedOutInteractable, LoggedOutPresentableListener {
+
+    weak var router: LoggedOutRouting?
+    weak var listener: LoggedOutListener?
+
+    // TODO: Add additional dependencies to constructor. Do not perform any logic
+    // in constructor.
+    override init(presenter: LoggedOutPresentable) {
+        super.init(presenter: presenter)
+        presenter.listener = self
+    }
+
+    override func didBecomeActive() {
+        super.didBecomeActive()
+        // TODO: Implement business logic here.
+    }
+
+    override func willResignActive() {
+        super.willResignActive()
+        // TODO: Pause any business logic.
+    }
+    
+    
+    // - MARK: LoggedOutPresentableListener
+    
+    func login(withPlayer1Name player1Name: String?, player2Name: String?) {
+        let play1NameWithDefault = playerName(player1Name, withDefaultName: "Player 1")
+        let play2NameWithDefault = playerName(player2Name, withDefaultName: "Player 2")
+        
+        print("\(play1NameWithDefault) vs \(play2NameWithDefault)")
+    }
+    
+    private func playerName(_ name: String?, withDefaultName defaultName: String) -> String {
+        if let name = name, !name.isEmpty {
+            return name
+        } else {
+            return defaultName
+        }
+    }
+}
